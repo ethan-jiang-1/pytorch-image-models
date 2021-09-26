535814a1a11a4d4d43be37519d10edac4412bdb3
ethanmac <ethanmac@mac.com>
2021-09-26 19:32:04 +0800

    update

--- inference_exp.py
+++ inference_exp.py
@@ -120,10 +120,11 @@ def main():
             #ethan add 4 :  save image
             print('input.shape', input.shape, batch_idx)
             im = input[0].cpu().numpy()
+            im = np.transpose(im, (1, 2, 0))
             print('im.shape', im.shape)
-            #img_input_filename = os.path.join(args.output_dir, "{}.png".format(batch_idx))
-            #im = Image.fromarray(im)
-            #im.save(img_input_filename)    
+            img_input_filename = os.path.join(args.output_dir, "{}.png".format(batch_idx))
+            im = Image.fromarray(im)
+            im.save(img_input_filename)    
 
             # measure elapsed time
             batch_time.update(time.time() - end)
