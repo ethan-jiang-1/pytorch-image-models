e829256f63b031ccafb785b9f1942c1e6b8f87d0
ethanmac <ethanmac@mac.com>
2021-10-19 15:49:06 +0800

    update

---
+++ train_exp_debug.py
@@ -0,0 +1,32 @@
+import sys
+import os
+#import shutil
+
+def _add_sys_path(new_sys_path):
+    if new_sys_path not in sys.path:
+        if os.path.isdir(new_sys_path):
+            print("add sys.path", new_sys_path)
+            sys.path.append(new_sys_path)
+
+def extend_sys_paths():
+    this_folder = os.path.dirname(__file__)
+    parent_folder = os.path.dirname(this_folder)
+
+    new_sys_path = parent_folder + "/pim"
+    _add_sys_path(new_sys_path)
+
+    new_sys_path = parent_folder 
+    _add_sys_path(new_sys_path)
+
+
+extend_sys_paths()
+
+
+def do_tain_exp():
+    from pim.train_exp import main
+    main()
+
+
+if __name__ == "__main__":
+    do_tain_exp()
+
